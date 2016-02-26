c435c272015f5477718b9e6fc1d86af691e5976a
Stephen Devine <stephend@pensaredevelopment.com>
2016-02-25 17:29:00 -0800

    Add some ui states

--- config.js
+++ config.js
@@ -1,18 +1,31 @@
 function config($ocLazyLoadProvider, $stateProvider, $urlRouterProvider) {
   console.log('ocLazyLoad config called');
 
+  $urlRouterProvider.otherwise('/login');
+
   $ocLazyLoadProvider.config({
-       // Set to true if you want to see what and when is dynamically loaded
-       debug: true
-   });
+    // Set to true if you want to see what and when is dynamically loaded
+    debug: true
+  });
 
-   
-
+  $stateProvider
+    .state('login', {
+      url: "/login",
+      templateUrl: "views/login.html",
+    })
+    .state('admin', {
+      url: "/admin",
+      templateUrl: "views/admin.html",
+    })
+    .state('userprofile', {
+      url: "/userprofile",
+      templateUrl: "views/userprofile.html",
+    });
 }
 
 angular
   .module('app')
   .config(config)
   .run(function($rootScope, $state) {
-          $rootScope.$state = $state;
-      });
+    $rootScope.$state = $state;
+  });

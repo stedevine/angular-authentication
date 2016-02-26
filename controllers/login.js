ca2fb47f23dcf2a02bb2e193a456c53dfdf5ef2a
Stephen Devine <stephend@pensaredevelopment.com>
2016-02-26 12:05:06 -0800

    Login as admin

--- controllers/login.js
+++ controllers/login.js
@@ -1,16 +1,31 @@
 function loginController($scope, $state, principal) {
-  $scope.login = function() {
 
-    // create an identity with a 'User' Role
-    principal.authenticate({
+  var logIn = function(principal, identity) {
+    principal.authenticate(identity);
+  }
+
+  $scope.adminLogin = function() {
+    var id = {
+      name: 'Administrator',
+      roles: ['User', 'Admin']
+    };
+    logIn(principal, id);
+    if ($scope.returnToState) $state.go($scope.returnToState.name, $scope.returnToStateParams);
+    else $state.go('admin');
+  }
+
+
+  $scope.userLogin = function() {
+    var id = {
       name: 'Test User',
       roles: ['User']
-    });
-
+    };
+    logIn(principal, id);
     if ($scope.returnToState) $state.go($scope.returnToState.name, $scope.returnToStateParams);
     else $state.go('userprofile');
-  }
+  };
 }
+
 /*
 angular
   .module('app')
